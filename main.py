a9ef2aa758d655583bb631d83b0d69510954f91b
farhan <farhan92cr@yahoo.com>
2025-07-01 15:46:43 +0500

    vulnerability scanning with Trivy added in deploy.yml file

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "ok"}
+    return {"status": "ok", " vulnerability scanning with Trivy added" : " " }
 
 @app.get("/health")
 def health_check():
