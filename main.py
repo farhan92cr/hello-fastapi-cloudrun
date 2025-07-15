d2d190d4d7009f351db5fef65ec60dcef4f1317a
farhan <farhan92cr@yahoo.com>
2025-07-15 11:44:52 +0500

    Added logging to FastAPI endpoints

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
+#from fastapi import FastAPI
+#app = FastAPI()
+
+#@app.get("/")
+#def read_root():
+ #   return {"status": "Video DEMO" }
+
+#@app.get("/health")
+#def health_check():
+ #   return {"status": "healthy"}
+
 from fastapi import FastAPI
+import logging
+
+# ✅ Configure logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("hello-fastapi")
+
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "OK" }
+    logger.info("📥 GET request received on '/' endpoint")
+    return {"status": "ok"}
 
 @app.get("/health")
 def health_check():
+    logger.info("💚 Health check passed")
     return {"status": "healthy"}
+
